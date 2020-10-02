95922c824cf49b29dcc04f835ec1c42af38dbfb2
Gustavo Castaneda <gustavocastaneda2323@gmail.com>
2020-10-01 23:55:17 -0700

    update

--- src/shared/forsale.js
+++ src/shared/forsale.js
@@ -3,29 +3,29 @@ export const FORSALE = [
         key: 0,
         src: "https://0.cdn.autotraderspecialty.com/1964-Ford-Mustang-american-classics--Car-101337900-ff32ab485e09af886a87447500d8c938.jpg?w=735&h=551&r=pad&c=%23f5f5f5",
         header: "1964 Mustang",
-        caption: "$3,000",
+        caption: "$210,000",
         location: "Oakland, CA",
         slides: [{
           src: 'https://0.cdn.autotraderspecialty.com/1964-Ford-Mustang-american-classics--Car-101337900-ff32ab485e09af886a87447500d8c938.jpg?w=735&h=551&r=pad&c=%23f5f5f5',
-          caption: '$3,000',
+          caption: '$210,000',
           header: '1964 Mustang'
         },
         {
           src: 'https://0.cdn.autotraderspecialty.com/1964-Ford-Mustang-american-classics--Car-101337900-3a209769ae14893a6de9105f7c683860.jpg?r=pad&w=735&h=551&c=%23f5f5f5',
-          caption: '$3,000',
+          caption: '$210,000',
           header: '1964 Mustang'
         },
         {
           src: 'https://0.cdn.autotraderspecialty.com/1964-Ford-Mustang-american-classics--Car-101337900-d00c48f583817800c18a37eb701b28d3.jpg?r=pad&w=735&h=551&c=%23f5f5f5',
-          caption: '$3,000',
+          caption: '$210,000',
           header: '1964 Mustang'
         }]
     },
     {
-        id: 1,
-        image: "https://www.roguefitness.com/media/catalog/product/cache/1/image/1500x1500/9df78eab33525d08d6e5fb8d27136e95/r/o/rouge-dumbbells-th_1.jpg",
-        name: "Dumbells",
-        price: "$10",
+        key: 1,
+        src: "https://www.roguefitness.com/media/catalog/product/cache/1/image/1500x1500/9df78eab33525d08d6e5fb8d27136e95/r/o/rouge-dumbbells-th_1.jpg",
+        header: "Dumbells",
+        caption: "$10",
         location: "Oakland, CA"
     }
 ]
